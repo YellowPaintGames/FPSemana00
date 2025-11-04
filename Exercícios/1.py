862c314d8662e839ffaec4b92548c0c0420b8acf
Francisco Caldeira <kikocaldeira2007@gmail.com>
2025-11-04 15:50:14 +0000

    Exercicio 2

--- Exercícios/1.py
+++
@@ -1,2 +0,0 @@
-hero=("John",1,10,0)
-print("O herói ",hero[0]," está no nível ",str(hero[1]),", tem",str(hero[2]),"de vida e ",str(hero[3]),"Xp")
---
+++ Exercícios/2.py
@@ -0,0 +1,3 @@
+Vector2=(0,0)
+x,y=Vector2
+print("A posição inicial do player é: x=",x,"y=",y)
